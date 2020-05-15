4876a0cb90c5ecf82eab19f288d2c5d4558a5622
Darvy F Ceron Gomez <darvy.ceron@gmail.com>
2020-05-15 16:51:04 -0500

    Roll back previous page

---
+++ build/precache-manifest.d401c9b38b9bf3612fa12dd245397a26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85ee70c0ddd510fd47a602a4af2ab3cd",
+    "url": "/darvycg.github.io/index.html"
+  },
+  {
+    "revision": "b5ed0d9cfd7b741b99de",
+    "url": "/darvycg.github.io/static/css/main.a25bac32.chunk.css"
+  },
+  {
+    "revision": "8b86b7134ef195e8fa4f",
+    "url": "/darvycg.github.io/static/js/2.a822b92e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/darvycg.github.io/static/js/2.a822b92e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5ed0d9cfd7b741b99de",
+    "url": "/darvycg.github.io/static/js/main.1dfc74d4.chunk.js"
+  },
+  {
+    "revision": "2ab2d8f949abe43f9534",
+    "url": "/darvycg.github.io/static/js/runtime-main.741e9120.js"
+  }
+]);
